7a955fc057a72bc6058f0cfadc082d7c4db37907
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-01-20 11:22:01 +0530

    welcome-section Done

---
+++ src/components/Authors.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Authors() {
+  return (
+    <div>
+      <h2 className="list-heading pb-2 border-bottom border-2 border-dark">
+        Authors
+      </h2>
+    </div>
+  );
+}
+
+export default Authors;
---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Books() {
+  return (
+    <div>
+      <h2 className="list-heading pb-2 border-bottom border-2 border-dark">
+        Books
+      </h2>
+    </div>
+  );
+}
+
+export default Books;
